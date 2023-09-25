819871ab705a12dd911d93f4fc48342dd4aeeeeb
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-09-25 12:06:55 +0100

    Save test code.

--- src/main.v
+++ src/main.v
@@ -1,20 +1,19 @@
 module main
 
-// import os
+import os
 import xml
 
 fn main() {
-	// paths := os.walk_ext('spec/local', 'xml')
+	paths := os.walk_ext('spec/local', 'xml')
 
-	// for path in paths {
-	// 	doc := xml.XMLDocument.parse_file(path) or {
-	// 		println('Failed to parse: ' + path)
-	// 		continue
-	// 	}
-	// 	println(doc)
-	// }
-	doc := xml.XMLDocument.parse_file('spec/local/01_mdn_example/hello_world.xml')!
-	// doc := xml.XMLDocument.parse_file('spec/local/03_cd_catalogue/cd_catalog.xml')!
-	println(doc)
-	// println(doc.root)
+	for path in paths {
+		doc := xml.XMLDocument.parse_file(path) or {
+			println('Failed to parse: ' + path)
+			continue
+		}
+		println(doc)
+	}
+	// doc := xml.XMLDocument.parse_file('spec/local/01_mdn_example/hello_world.xml')!
+	// // doc := xml.XMLDocument.parse_file('spec/local/08_comments/comment.xml')!
+	// println(doc)
 }
