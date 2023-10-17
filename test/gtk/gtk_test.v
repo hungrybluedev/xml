deab912eac599241b1a8bb5c4fa886ad4d3e6fad
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-10-17 08:40:07 +0100

    fix: Return error instead of assert false

--- test/gtk/gtk_test.v
+++ test/gtk/gtk_test.v
@@ -5,7 +5,9 @@ import os
 
 fn test_large_gtk_file() ! {
 	path := os.join_path(os.dir(@FILE), 'Gtk-4.0.gir')
-	actual := xml.XMLDocument.from_file(path) or { assert false, 'Failed to parse large XML file' }
+	actual := xml.XMLDocument.from_file(path) or {
+		return error('Failed to parse large GTK XML file')
+	}
 
 	assert true
 }
