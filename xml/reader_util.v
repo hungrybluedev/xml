b5c1d0c2e70e7ab145a07781bd81a6de4ddd72f5
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-10-17 15:29:12 +0100

    refactor: Use FullBufferReader in favour of StringReader

--- xml/reader_util.v
+++ xml/reader_util.v
@@ -3,28 +3,29 @@ module xml
 import io
 import math
 
-struct StringReader {
-	contents string
-mut:
-	position int
-}
-
-fn (mut sr StringReader) read(mut buf []u8) !int {
-	if sr.position >= sr.contents.len {
-		return 0
-	}
-	n := math.min(buf.len, sr.contents.len - sr.position)
-	for i := 0; i < n; i++ {
-		buf[i] = sr.contents[sr.position + i]
-	}
-	sr.position += n
-	return n
-}
-
 fn next_char(mut reader io.Reader) !u8 {
 	mut buf := [u8(0)]
 	if reader.read(mut buf)! == 0 {
 		return error('Unexpected End Of File.')
 	}
 	return buf[0]
+}
+
+struct FullBufferReader {
+	contents []u8
+mut:
+	position int
+}
+
+[direct_array_access]
+fn (mut fbr FullBufferReader) read(mut buf []u8) !int {
+	if fbr.position >= fbr.contents.len {
+		return 0
+	}
+	n := math.min(buf.len, fbr.contents.len - fbr.position)
+	for i := 0; i < n; i++ {
+		buf[i] = fbr.contents[fbr.position + i]
+	}
+	fbr.position += n
+	return n
 }
