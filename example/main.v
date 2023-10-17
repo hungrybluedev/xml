a1aef5eb92e7329ce8da4cb6576a28822b8ca7e6
Subhomoy Haldar <hungrybluedev@gmail.com>
2023-10-17 13:28:22 +0100

    feat: measure timing using stopwatch

--- example/main.v
+++ example/main.v
@@ -2,6 +2,7 @@ module main
 
 // import os
 import xml
+import time
 
 fn main() {
 	// paths := os.walk_ext('src/spec/local', 'xml')
@@ -15,6 +16,15 @@ fn main() {
 	// 	println('\n\n')
 	// }
 	// doc := xml.XMLDocument.from_file('test/local/01_mdn_example/hello_world.xml')!
+	mut sw := time.new_stopwatch()
 	doc := xml.XMLDocument.from_file('test/gtk/Gtk-4.0.gir')!
-	println(doc)
+	println('Parsing took ${sw.elapsed().milliseconds()} ms')
+
+	sw.restart()
+	validated := doc.validate()!
+	println('Validation took ${sw.elapsed().milliseconds()} ms')
+
+	sw.restart()
+	println(validated.get_elements_by_tag('include'))
+	println('Getting elements took ${sw.elapsed().milliseconds()} ms')
 }
